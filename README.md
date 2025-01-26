3d09c459d4f9598df5bfb17f7113a3ff4f099eac
Alexander Lazarov <alzlaz.learns@gmail.com>
2025-01-26 16:43:26 -0600

    feat: added mockito to maven and wrote a sample test to show its working.

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# Agile459-ROGUE-Exploring-the-Dungeons-of-Doom
 
-## Group
-| Name   | Introduction                          |
-|--------|---------------------------------------|
-| Alex   |  |
-| Uday   |  |
-| Dillon | I am a current web developer with exeperience in Java, Python, and Sql. Interested in artificial intelligence, robotics, and web applications |
--- dungeonofdoom/src/test/java/helloworldtest/HelloWorldTest.java
+++ dungeonofdoom/src/test/java/helloworldtest/HelloWorldTest.java
@@ -1,8 +1,11 @@
 package helloworldtest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import com.example.helloworld.HelloWorld;
 
@@ -10,5 +13,16 @@ public class HelloWorldTest {
     @Test
     public void testAddPass() {
         assertEquals("Hello World", HelloWorld.test());
+    }
+
+    @Test
+    public void testMock(){
+        try (MockedStatic<HelloWorld> mockedHelloWorld = mockStatic(HelloWorld.class)) {
+            mockedHelloWorld.when(HelloWorld::test).thenReturn("Mocked Hello World");
+
+            assertEquals("Mocked Hello World", HelloWorld.test());
+
+            mockedHelloWorld.verify(HelloWorld::test, times(1));
+        }
     }
 }
