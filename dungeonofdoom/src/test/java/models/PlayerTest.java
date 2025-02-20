0862a6557aecc24ba12bc44312338738aa87f19d
Alexander Lazarov <alzlaz.learns@gmail.com>
2025-02-19 20:13:45 -0600

    changed how player works so needed to change basic test of player.

--- dungeonofdoom/src/test/java/models/PlayerTest.java
+++ dungeonofdoom/src/test/java/models/PlayerTest.java
@@ -16,7 +16,7 @@ public class PlayerTest {
         System.out.println("Exp: " + player.getExperience());
         
         String expected = """
-        Level: 1	Hits: 100 (100)	Str: 3	Gold: 0	Armor: 0	Exp: 1/0""";
+        Level: 1	Hits: 100 (100)	Str: 3	Gold: 0	Armor: 5	Exp: 1/0""";
             
             //changed format to fit in message bar updating test
             // """
