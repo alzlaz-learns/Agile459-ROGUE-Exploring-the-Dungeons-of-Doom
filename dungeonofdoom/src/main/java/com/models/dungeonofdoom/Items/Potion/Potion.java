ff0047f21fe2b59f27cecd0f1d1be7bef51c2a09
Dillon G <d777g@protonmail.com>
2025-03-15 17:36:55 -0700

    fix Potion

--- dungeonofdoom/src/main/java/com/models/dungeonofdoom/Items/Potion/Potion.java
+++ dungeonofdoom/src/main/java/com/models/dungeonofdoom/Items/Potion/Potion.java
@@ -2,6 +2,7 @@ package com.models.dungeonofdoom.Items.Potion;
 
 import com.models.Player;
 import com.models.dungeonofdoom.Items.Item;
+import com.models.dungeonofdoom.Items.ItemOptions;
 import com.models.dungeonofdoom.dungeonfloor.DungeonFloor;
 import com.models.dungeonofdoom.enums.PotionEnum;
 import com.models.dungeonofdoom.monster.Monster;
@@ -51,6 +52,11 @@ public class Potion extends Item{
     @Override
     public String getItemName(){
         return isIdentified() ? "Potion of " + type.getName() : "Potion of " + type.getScramble();
+    }
+
+    @Override
+    public ItemOptions getItemOption() {
+        return ItemOptions.QUAFFABLE;
     }
     
 }
